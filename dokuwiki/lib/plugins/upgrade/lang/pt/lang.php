889ff9271a5888849edf12367d689d4d7e60c0bb
Shane Drafahl <shanedrafahl@gmail.com>
2019-06-13 23:36:53 -0500

    Added

---
+++ dokuwiki/lib/plugins/upgrade/lang/pt/lang.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author José Vieira <jmsv63@gmail.com>
+ * @author Oze Projetos <oze@oze.net.br>
+ */
+$lang['menu']                  = 'Atualização Wiki';
+$lang['vs_php']                = 'A nova versão de DokuWiki necessita pelo menos de PHP %s, mas está utilizando %s.
+Terá de atualizar a sua versão do PHP antes de continuar a atualização!';
+$lang['vs_tgzno']              = 'Não foi possível determinar a versão mais recente de DokuWiki.';
+$lang['vs_tgz']                = 'DokuWiki <b>%s</b> está disponível para descarregar.';
+$lang['vs_local']              = 'Atualmente está a executar DokuWiki  <b>%s</b>.';
+$lang['vs_localno']            = 'Não é claro qual a sua versão, sendo recomendada uma atualização manual.';
+$lang['vs_newer']              = 'Parece que seu DokuWiki  é mais recente que a última versão estável. Atualização não recomendável.';
+$lang['vs_same']               = 'Seu DokuWiki já é a versão mais recente. Não necessita de atualização.';
+$lang['vs_plugin']             = 'Há uma nova atualização de plugin disponível (%s) você deve atualizar o plugin antes de continuar.';
+$lang['vs_ssl']                = 'Seu PHP parece não suportar SSL, download da informação necessária irá falhar. Atualize manualmente.';
+$lang['dl_from']               = 'Descarregando arquivo de %s...';
+$lang['dl_fail']               = 'Descarga falhou.';
+$lang['dl_done']               = 'Descarga completa (%s).';
+$lang['pk_extract']            = 'Descompactando...';
+$lang['pk_fail']               = 'Falha de descompactação.';
+$lang['pk_done']               = 'Descompactação completada.';
+$lang['pk_version']            = 'DokuWiki  <b>%s</b> já está instalado (Você está executando a versão <b>%s</b>).';
+$lang['ck_start']              = 'Verificando permissões de arquivo ...';
+$lang['ck_done']               = 'Todos os arquivos são graváveis. Pronto para atualizar.';
+$lang['ck_fail']               = 'Alguns arquivos não são graváveis. Não é possível a atualização automática.';
+$lang['cp_start']              = 'Atualizando arquivos ...';
+$lang['cp_done']               = 'Todos os arquivos atualizados.';
+$lang['cp_fail']               = 'Uh-Oh. Algo deu errado. Melhor verificar manualmente.';
+$lang['tv_noperm']             = '<code>%s</code> não é gravável!';
+$lang['tv_upd']                = '<code>%s</code> será atualizado.';
+$lang['tv_nocopy']             = 'Não foi possível copiar o arquivo <code>%s</code>!';
+$lang['tv_nodir']              = 'Não foi possível criar o diretório <code>%s</code>!';
+$lang['tv_done']               = '<code>%s</code> atualizado!';
+$lang['rm_done']               = '<code>%s</code> obsoleto foi eliminado.';
+$lang['rm_fail']               = 'Não foi possível excluir <code>%s</code> obsoleto. <B> Por favor, eliminar manualmente! </ B>';
+$lang['rm_mismatch']           = 'Incompatibilidade com maiúsculas ou minúsculas no ficheiro obsoleto <code>%s</code>. Por favor verificar manualmente se este ficheiro deverá ser eliminado.';
+$lang['finish']                = 'Atualizar concluída. Aproveite o seu novo DokuWiki';
+$lang['btn_continue']          = 'Continue';
+$lang['btn_abort']             = 'Abortar';
+$lang['step_version']          = 'Conferir';
+$lang['step_download']         = 'Download';
+$lang['step_unpack']           = 'Descompactar';
+$lang['step_check']            = 'Verificar';
+$lang['step_upgrade']          = 'Instalar';
+$lang['careful']               = 'Erros acima! Você <b>não</b> deve continuar!';
